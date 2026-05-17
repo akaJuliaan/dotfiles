40f2d6867c2707c82c0f082215ff77839dd342d3
akaJuliaan <hi@julian.at>
2026-05-17 19:51:10 +0200

    fix media prev button

--- core/.config/hypr/keybinds.lua
+++ core/.config/hypr/keybinds.lua
@@ -76,4 +76,4 @@ hl.bind("XF86MonBrightnessDown", hl.dsp.exec_cmd(osd .. " --brightness lower"), 
 hl.bind("XF86AudioNext", hl.dsp.exec_cmd(osd .. " --playerctl next"), { locked = true })
 hl.bind("XF86AudioPause", hl.dsp.exec_cmd(osd .. " --playerctl play-pause"), { locked = true })
 hl.bind("XF86AudioPlay", hl.dsp.exec_cmd(osd .. " --playerctl play-pause"), { locked = true })
-hl.bind("XF86AudioPrev", hl.dsp.exec_cmd(osd .. "--playerctl previous"), { locked = true })
+hl.bind("XF86AudioPrev", hl.dsp.exec_cmd(osd .. " --playerctl previous"), { locked = true })
